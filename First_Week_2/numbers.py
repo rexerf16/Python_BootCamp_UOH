e1a4c167f39c02171108307a04c5167232f302a1
Mohammed Farhan <86636267+rexerf16@users.noreply.github.com>
2023-01-09 23:17:56 +0300

    Create numbers.py

---
+++ First_Week_2/numbers.py
@@ -0,0 +1,20 @@
+# *1 math functions in python
+# printing an integer with a string
+from math import*
+a = 5
+print("This number is integer "+str(a))
+# printing an abslute value of a giving integer
+b = -7
+print(abs(b))
+# printing a power of an integeer
+print(pow(3, 4))
+# returns the biggest number between the givin integeers
+print(max(5, 10))
+# it will round the number to the nearest real number
+print(round(3.4))
+# Floors the decimal number
+print(floor(3.6))
+# Ceils the decimal number
+print(ceil(4.1))
+# returns the square root of a givin number
+print(sqrt(55))
