7072c14a593e72654f461a141564b9e3530fa2e1
Mohammed Farhan <86636267+rexerf16@users.noreply.github.com>
2023-01-30 17:31:01 +0300

    Add files via upload

---
+++ First_Week_3/functions.py
@@ -0,0 +1,16 @@
+# *1 Functions
+# defineing a function then calling it
+def say_hello():
+    print("Hello User you called this method !!")
+
+
+say_hello()
+
+# defineing a function then calling it with passing a parmeter
+
+
+def receiving(name, age):
+    print("i got this name and age from the call parameter >>> "+name+" >>> "+age)
+
+
+receiving("Mohammed", "23")
---
+++ First_Week_3/return.py
@@ -0,0 +1,17 @@
+# *2 Return statment
+# sending back a function result to the caller
+def add(num):
+    return num+num+num
+    print("code")
+
+
+print(add(3))
+
+
+def multi(num):
+    return num*num*num
+    print("code")
+
+
+result = multi(9)
+print(result)
